f1f9a7af3e7ddd6ae8f470f3af264c515806a1e3
michael-maher1999 <michaelmaher98765@gmail.com>
2022-10-03 01:13:48 +0200

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+public function all(){
+    $user= User::all();
+    return response()->json(compact('user'));
+}
+}
